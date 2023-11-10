7014723a01db8787042559a440170127e3dbf7a9
Atai_Mamytov <atay.mamytov@gmail.com>
2023-11-10 11:59:13 +0100

    Search field for choose product

--- lib/presentation/choose_product_page/choose_product_list/choose_product_list_view.dart
+++ lib/presentation/choose_product_page/choose_product_list/choose_product_list_view.dart
@@ -10,6 +10,32 @@ class ChoodeProductListView extends StatefulWidget {
 class _ChoodeProductListViewState extends State<ChoodeProductListView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Column(
+        children: [
+          TextField(
+            onChanged: (value) {
+              // Handle the search query when the user types
+              // You can use the 'value' variable to perform search operations
+            },
+            decoration: InputDecoration(
+              fillColor: Color(0xFFEDF0F8),
+              filled: true,
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12.0), // Border radius
+                borderSide: BorderSide(
+                  color: Colors.transparent, // Transparent border color
+                ),
+              ),
+
+              hintText: 'Search...', // Placeholder text
+              prefixIcon: Icon(Icons.search), // Icon to indicate search
+              border: OutlineInputBorder(), // Border around the text field
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
